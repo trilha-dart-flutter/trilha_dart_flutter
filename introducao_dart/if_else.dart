bdbcd7e89e6e926a0063f6f713e27b99f8a7f2a1
Adelmo Menezes - narnia <adelmo.msp@gmail.com>
2022-06-26 22:00:29 -0300

    Curso de Dart - [03] Estruturas de Fluxo

---
+++ introducao_dart/if_else.dart
@@ -0,0 +1,13 @@
+void main() {
+  bool seguirEmFrente = false;
+
+  if (seguirEmFrente) {
+    print("Andar");
+  } else {
+    print("Parar");
+  }
+
+  if (10 > 5) {
+    print("10 eh maior que 5");
+  }
+}
---
+++ introducao_dart/swirch.dart
@@ -0,0 +1,17 @@
+void main() {
+  int valorInt = 2;
+
+  switch (valorInt) {
+    case 0:
+      print("ZERO");
+      break;
+    case 1:
+      print("UM");
+      break;
+    case 2:
+      print("dois");
+      break;
+    default:
+      print("PADRÂO");
+  }
+}
