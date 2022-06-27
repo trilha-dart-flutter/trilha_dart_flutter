69294de269bb9a1031aef3b9aad74754f73d4601
Adelmo Menezes - narnia <adelmo.msp@gmail.com>
2022-06-27 13:31:15 -0300

    Curso de Dart - [11] Callable Interface (CALL)

---
+++ introducao_dart/callable_interface.dart
@@ -0,0 +1,9 @@
+void main() {
+  BuscarAlunos buscarAlunos = BuscarAlunos();
+  buscarAlunos();
+}
+
+class BuscarAlunos {
+  // void buscar() => print("Adelmo, Menezes, Silva");
+  void call() => print("Adelmo, Menezes, Silva");
+}
