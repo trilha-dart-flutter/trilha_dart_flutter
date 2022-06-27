44013117a5a211d247aba27d5d9c8e0f65c9120b
Adelmo Menezes - narnia <adelmo.msp@gmail.com>
2022-06-26 21:36:46 -0300

    Curso de Dart - [01] Variáveis e Tipos

---
+++ introducao_dart/variaveis.dart
@@ -0,0 +1,13 @@
+void main() {
+  String variavelNome = "Adelmo Menezes";
+  print(variavelNome);
+
+  int valorVariavel = 10;
+  print(valorVariavel);
+
+  bool ehVerdadeiro = true;
+  print(ehVerdadeiro);
+
+  List<String> listaDePalavras = ["Adelmo", "Menezes"];
+  print("${listaDePalavras[0]} - ${listaDePalavras[1]}");
+}
